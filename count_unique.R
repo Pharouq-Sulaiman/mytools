abaa777252d267ca4f09bc832d2bf1ee47c0a8e9
Pharouq-Sulaiman <141918302+Pharouq-Sulaiman@users.noreply.github.com>
2025-05-04 01:31:30 +0100

    Update count_unique.R

--- count_unique.R
+++ count_unique.R
@@ -1,9 +1,14 @@
-
-count_unique <- function(data, i) {
-  for (i in colnames(data)) {
-    cat("Column:", i, "\n")
-    print(summary(data.frame(unique(house_dat[[i]]))))
-    cat("\n\n")
-  }
+#' Count Unique Values
+#'
+#' This function summarizes unique values in each column of a data frame.
+#'
+#' @param data A data frame.
+#' @return A data frame with column names and count of unique values.
+#' @export
+count_unique <- function(data) {
+  data.frame(
+    Column = colnames(data),
+    Unique_Count = sapply(data, function(x) length(unique(x))),
+    stringsAsFactors = FALSE
+  )
 }
-
