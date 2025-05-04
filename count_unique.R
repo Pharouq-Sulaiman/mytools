d0a8ed064d9b17aaed0da3f69ede27d88e98bc70
Pharouq-Sulaiman <141918302+Pharouq-Sulaiman@users.noreply.github.com>
2025-05-04 01:29:33 +0100

    Update count_unique.R

--- count_unique.R
+++ count_unique.R
@@ -1,11 +1,4 @@
-#Summarize Unique Values by Column
-#'
-#' This function summarizes the unique values in each column of a data frame.
-#'
-#' @param df A data frame.
-#'
-#' @return A data frame showing summary statistics of unique values per column.
-#' @export
+
 count_unique <- function(data, i) {
   for (i in colnames(data)) {
     cat("Column:", i, "\n")
